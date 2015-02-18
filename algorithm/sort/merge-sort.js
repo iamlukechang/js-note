36eee1c882211f9bb57e857e1fc1da677128f85f
Luke Chang <iamlukechang@gmail.com>
2015-02-18 10:38:51 -0800

    make merge sort simple

--- algorithm/sort/merge-sort.js
+++ algorithm/sort/merge-sort.js
@@ -11,38 +11,39 @@
  *   1. Set boundary
  *   2. Divide the arr into two parts
  *     1. Do merge sort to each part // N = N/2
- *     2. Merge the two sorted parts. // O(N)
+ *
+ *      Note: The space complexity O(N) is used here (arr.slice), if we don't
+ *            use an extra array to save the result, but just do merge two
+ *            parts in the same array, it will become a sorted array insert
+ *            problem, which cost an O(N) time complexity. Using linked list
+ *            can avoid the extra space here.
+ *
+ *     2. Merge the two sorted parts into a sorted one. // O(N)
  *
  * @author iamlukechang@gmail.com (Luke Chang)
  */
 
-function mergeSort(arr, start, end) {
-  start = start || 0; 
-  end = end || arr.length;
-  var len = end - start;
+function mergeSort(arr) {
+  var len = arr.length;
   // step 1
-  if (len == 1) return arr.slice(start, end);
+  if (len == 1) return arr;
 
-  var middle = start + Math.floor(len / 2);
+  var mid = Math.floor(len / 2);
 
-  // step 1.1 & step 1.2
-  return merge(mergeSort(arr, start, middle), mergeSort(arr, middle, end));
+  // step 1.1
+  return merge(mergeSort(arr.slice(0, mid)), mergeSort(arr.slice(mid, len)));
 }
 
 function merge(arr1, arr2) {
-  // The space complexity O(N) is used here(and the arr.slice(0, 1) at step 1),
-  // if we don't use an extra array to save the result, but just do merge
-  // two parts in the same array, it will become a sorted array insert problem,
-  // which cost an O(N) time complexity
   var rst = []
-  var cnt1 = cnt2 = 0;
-  var len1 = arr1.length,
-      len2 = arr2.length;
-  while (cnt1 < len1 || cnt2 < len2) {
-    if (arr1[cnt1] < arr2[cnt2] || cnt2 == len2) {
-      rst.push(arr1[cnt1++]);
-    } else if (arr2[cnt2] <= arr1[cnt1] || cnt1 == len1) {
-      rst.push(arr2[cnt2++]);
+  var len = arr1.length + arr2.length;
+
+  // step 1.2
+  while (rst.length < len) {
+    if (arr1[0] <= arr2[0] || arr2.length == 0) {
+      rst.push(arr1.shift());
+    } else if (arr2[0] < arr1[0] || arr1.length == 0) {
+      rst.push(arr2.shift());
     }
   }
 
